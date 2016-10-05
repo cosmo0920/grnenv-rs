fd77bc52bd9009884e2701c35344cde5b6f45c41
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2016-10-05 23:54:08 +0900

    unix profile: Support more environment variables
    * LD_LIBRARY_PATH | for Linux
    * DYLD_LIBRARY_PATH | for macOS
    * PKG_CONFIG_PATH

--- src/profile/unix.rs
+++ src/profile/unix.rs
@@ -8,6 +8,14 @@ pub fn create_profile_source(shim_dir: &PathBuf,
                              groonga_dir: &String,
                              install_dir: &PathBuf)
                              -> Result<(), io::Error> {
+    #[cfg(not(target_os = "macos"))]
+    fn dynamic_link_var_pathname() -> String {
+        "LD_LIBRARY_PATH".to_string()
+    }
+    #[cfg(target_os = "macos")]
+    fn dynamic_link_var_pathname() -> String {
+        "DYLD_LIBRARY_PATH".to_string()
+    }
     if shim_dir.join("source-groonga.sh").exists() {
         let _ = try!(fs::remove_file(shim_dir.join("source-groonga.sh")));
     }
@@ -15,12 +23,21 @@ pub fn create_profile_source(shim_dir: &PathBuf,
         .expect("Could not create a shell setting file.");
     let installed_groonga = install_dir.join(groonga_dir)
         .join("bin");
+    let libgroonga = install_dir.join(groonga_dir)
+        .join("lib");
+    let pkg_config = install_dir.join(groonga_dir)
+        .join("lib")
+        .join("pkgconfig");
     if !installed_groonga.exists() {
         println!("Specified version is not installed.");
         process::exit(1);
     }
-    let contents = format!("export PATH={}:$PATH",
-                           installed_groonga.display());
+    let contents = format!("export PATH={}:$PATH\nexport {}={}:${}\nexport PKG_CONFIG_PATH={}:$PKG_CONFIG_PATH",
+                           installed_groonga.display(),
+                           dynamic_link_var_pathname(),
+                           libgroonga.display(),
+                           dynamic_link_var_pathname(),
+                           pkg_config.display());
     match f.write_all(&contents.as_bytes()) {
         Ok(_) => return Ok(()),
         Err(e) => println!("{}", e),
