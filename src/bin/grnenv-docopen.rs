26df11786a9ecc0f9c22a06bdd945cfc32b2ef35
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2016-10-16 18:17:01 +0900

    docopen: Add docopen subcommand

---
+++ src/bin/grnenv-docopen.rs
@@ -0,0 +1,23 @@
+use std::process;
+use std::process::Command;
+
+fn main () {
+    #[cfg(target_os = "macos")]
+    fn inner() -> &'static str {
+        "open"
+    }
+
+    let command = inner();
+    let arg = "http://groonga.org/docs/";
+    let err = match Command::new(command)
+        .args(&[arg])
+        // .stdout(Stdio::inherit())
+        // .stderr(Stdio::inherit())
+        .spawn() {
+        Ok(_) => return (),
+        Err(e) => e,
+        };
+    println!("Failed to execute docopen subcommand. reason: {:?}", err);
+    process::exit(1);
+
+}
