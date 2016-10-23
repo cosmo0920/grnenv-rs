9d644cbf421b8fe7f3d7a710d712c9270a91c7c7
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2016-10-23 14:14:50 +0900

    config: Add Config::new() test case

--- src/config.rs
+++ src/config.rs
@@ -3,7 +3,7 @@ use std::borrow::Cow;
 use clap::ArgMatches;
 use util;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Config<'a> {
     pub install_dir: PathBuf,
     pub shim_dir: PathBuf,
@@ -61,5 +61,47 @@ impl<'a> Config<'a> {
             version: version,
             build_conf: build_conf,
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::env;
+    use std::path::PathBuf;
+
+    #[cfg(target_os = "linux")]
+    fn stub_home() -> &'static str {
+        "/home/grnenv"
+    }
+    #[cfg(target_os = "macos")]
+    fn stub_home() -> &'static str {
+        "/Users/grnenv"
+    }
+    #[cfg(target_os = "windows")]
+    fn stub_home() -> &'static str {
+        "C:\\Users\\grnenv"
+    }
+
+    #[test]
+    fn test_config() {
+        let home = env::home_dir().unwrap();
+        let stub_home = stub_home();
+        let _ = env::set_var("HOME", stub_home.clone());
+        let install_dir = PathBuf::from(stub_home.clone()).join(".groonga");
+        let versions_dir = PathBuf::from(stub_home.clone()).join(".groonga").join("versions");
+        let shim_dir = PathBuf::from(stub_home.clone()).join(".groonga").join("shims").join("bin");
+        let build_conf = PathBuf::from(stub_home.clone()).join(".groonga").join("build.toml");
+        let expected = Config {
+            install_dir: install_dir,
+            shim_dir: shim_dir,
+            versions_dir: versions_dir,
+            arch: None,
+            version: None,
+            build_conf: build_conf,
+        };
+        let config = Config::new();
+        assert_eq!(expected, config);
+        let _ = env::set_var("HOME", home);
     }
 }
